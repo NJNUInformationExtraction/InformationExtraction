9265861d9701dc5e590f42c3ee1664e22183e3c5
song+ <Administrator@CN-20150509ADWZ>
2016-01-10 19:38:27 +0800

    内容部分只取前面部分，后面部分删除。

--- InformationExtraction/src/cn/edu/njnu/infoextract/impl/activities/Tool/Tool_File.java
+++ InformationExtraction/src/cn/edu/njnu/infoextract/impl/activities/Tool/Tool_File.java
@@ -4,12 +4,25 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class Tool_File {
+	
+    public static void writeFile(String fileName, String content) {
+        try {
+            //打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
+            FileWriter writer = new FileWriter(fileName, true);
+            writer.write(content);
+            writer.write("\r\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	public static ArrayList<String> readFolder(String File_Direction) {	
 		ArrayList<String> files=new ArrayList<String>();
